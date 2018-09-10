08aa5bae9613a133b80863abccfc8329984eefea
kepoorhampond <kepoorh@gmail.com>
2018-09-10 09:25:13 +0000

    Pokeball

---
+++ Pokeball.java
@@ -0,0 +1,19 @@
+/*
+ * This program prints an ascii pokeball.
+ */
+
+public class Pokeball {
+	public static void main (String[] args) {
+
+		System.out.println("   @@@@@@@@@@");
+		System.out.println("  @@@@@@@@@@@@");
+		System.out.println(" @@@@@@@@@@@@@@");
+		System.out.println("@@@@@@@@@@@@@@@@");
+		System.out.println("|||||[““““]|||||"); // These are special quotes
+		System.out.println("|||||[....]|||||");
+		System.out.println("OOOOOOOOOOOOOOOO");
+		System.out.println(" OOOOOOOOOOOOOO");
+		System.out.println("  OOOOOOOOOOOO");
+		System.out.println("   OOOOOOOOOO");
+	}
+}
